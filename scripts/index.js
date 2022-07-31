31e7d3f8ba5964d206528035a0fbb8f66d017bd7
Xenia Antipova <ksuskaant@yandex.ru>
2022-07-31 21:36:03 +0300

    исправлены ошибки после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
-const popup = document.querySelector(".popup"); // попап
-const btnPopupOpen = document.querySelector(".profile__btn-edit"); // кнопка редактирования профиля
-const btnPopupClose = popup.querySelector(".popup__close"); // кнопка закрытия попапа
+const popupProfile = document.querySelector(".popup_profile"); // попап
+const btnPopupProfileOpen = document.querySelector(".profile__btn-edit"); // кнопка редактирования профиля
+const btnPopupProfileClose = popupProfile.querySelector(".popup__close"); // кнопка закрытия попапа
 const profileNameInput = document.querySelector(".popup__input_value_name"); // инпут имени в  попапе
 //инпут описания в попапе
 const profileDescriptionInput = document.querySelector(
   ".popup__input_value_description"
 );
-const editForm = document.querySelector(".popup__form"); // форма в попапе
+const popupProfileForm = document.querySelector(".popup__form"); // форма в попапе
 const profileName = document.querySelector(".profile__name"); // имя в профиле на странице
 const profileDescription = document.querySelector(".profile__subtitle"); // описание в профиле на странице
 const photolist = document.querySelector(".photo__list"); // список фотокарточек
@@ -16,7 +16,7 @@ const btnPopupCardAddOpen = document.querySelector(".profile__btn-add"); // кн
 const btnPopupCardAddClose = popupCardAdd.querySelector(".popup__close"); // кнопка закрытия попапа добавления карточки
 const cardNameInput = document.querySelector(".popup__input_value_header"); // инпут названия карточки в  попапе
 const cardLinkInput = document.querySelector(".popup__input_value_link"); //инпут ссылки карточки в попапе
-const editFormCardAdd = document.querySelector(".popup__form_card-add"); // форма в попапе
+const popupProfileFormCardAdd = document.querySelector(".popup__form_card-add"); // форма в попапе
 const popupPhoto = document.querySelector(".popup_photo"); // попап открытия фотографии
 const popupName = popupPhoto.querySelector('.popup__name'); // название фотографии в попапе
 const popupPhotoImg = popupPhoto.querySelector('.popup__img'); // фотография в попапе
@@ -56,14 +56,12 @@ initialCards.forEach(function (element) {
 });
 
 // функция открытия попапа
-function openPopup() {
+function openPopup(popup) {
   popup.classList.add("popup_opened"); // добавляем класс popup_opened попапу для видимости
-  profileNameInput.value = profileName.textContent; // заполняем инпут имени в попапе именем из профиля
-  profileDescriptionInput.value = profileDescription.textContent; // заполняем инпут описания в попапе описанием из профиля
 }
 
 // функция закрытия попапа
-function closePopup() {
+function closePopup(popup) {
   popup.classList.remove("popup_opened"); // удаляем класс popup_opened попапу для скрытия
 }
 
@@ -72,21 +70,11 @@ function updateProfile(evt) {
   evt.preventDefault(); // сбрасываем событие отправки формы
   profileName.textContent = profileNameInput.value; // заполняем имя в профиле значением инпута имени из попапа
   profileDescription.textContent = profileDescriptionInput.value; // заполняем описание в профиле значением инпута описания из попапа
-  closePopup(); //вызываем функцию закрытия попапа
-}
-
-// функция открытия попапа для добавления карточки
-function openPopupCardAdd() {
-  popupCardAdd.classList.add("popup_opened"); // добавляем класс popup_opened попапу для видимости
-}
-
-// функция закрытия попапа  для добавления карточки
-function closePopupCardAdd() {
-  popupCardAdd.classList.remove("popup_opened"); // удаляем класс popup_opened попапу для скрытия
+  closePopup(popupProfile); //вызываем функцию закрытия попапа
 }
 
 // функция добавления карточки на страницу
-function cardAdd(evt) {
+function submitAddCardForm(evt) {
   evt.preventDefault(); // сбрасываем событие отправки формы
 
   // определяем объект карточки
@@ -97,7 +85,13 @@ function cardAdd(evt) {
 
   //вызываем функцию добавления карточки
   addNewCard(element);
-  closePopupCardAdd(); //вызываем функцию закрытия попапа
+  closePopup(popupCardAdd);
+  cardNameInput.value = '';
+  cardLinkInput.value = '';
+}
+
+function renderCard(card, container) {
+  container.prepend(card); // отображаем на странице
 }
 
 //добавление новой карточки
@@ -125,22 +119,34 @@ function addNewCard(element) {
     const name = event.target.nextElementSibling.querySelector('.photo__name').textContent; // находим имя фотокарточки
     const img = event.target.src; // достаем ссылку на фотокарточку
 
-    popupPhoto.classList.add("popup_opened"); // добавляем класс popup_opened попапу для видимости
+    openPopup(popupPhoto);
     popupName.textContent = name; //заменяем в попапе имя на имя найденной фотокарточки
     popupPhotoImg.src = img; // заменяем в попапе картинку
   });
 
-  photolist.prepend(initialCard); // отображаем на странице
+  renderCard(initialCard, photolist);
 }
 
-btnPopupOpen.addEventListener("click", openPopup); // обработчик события клика по кнопке редактирования профиля
-btnPopupClose.addEventListener("click", closePopup); // обработчик события клика по кнопке закрытия попапа
-editForm.addEventListener("submit", updateProfile); // обработчик события отправки формы сохранения профиля
+btnPopupProfileOpen.addEventListener("click", function() {
+  openPopup(popupProfile);
+  profileNameInput.value = profileName.textContent; // заполняем инпут имени в попапе именем из профиля
+  profileDescriptionInput.value = profileDescription.textContent; // заполняем инпут описания в попапе описанием из профиля
+});
 
-btnPopupCardAddOpen.addEventListener("click", openPopupCardAdd); // обработчик события клика по кнопке редактирования профиля
-btnPopupCardAddClose.addEventListener("click", closePopupCardAdd); // обработчик события клика по кнопке закрытия попапа
-editFormCardAdd.addEventListener("submit", cardAdd); // обработчик события отправки формы сохранения профиля
+// обработчик события клика по кнопке редактирования профиля
+btnPopupProfileClose.addEventListener("click", function() {
+  closePopup(popupProfile);
+}); // обработчик события клика по кнопке закрытия попапа
+popupProfileForm.addEventListener("submit", updateProfile); // обработчик события отправки формы сохранения профиля
 
-btnPopupPhotoClose.addEventListener("click", function() { // закрываем попап с фотографией по клику
-  popupPhoto.classList.remove("popup_opened");
+btnPopupCardAddOpen.addEventListener("click", function() {
+  openPopup(popupCardAdd);
+}); // обработчик события клика по кнопке редактирования профиля
+btnPopupCardAddClose.addEventListener("click", function() {
+  closePopup(popupCardAdd)
+}); // обработчик события клика по кнопке закрытия попапа
+popupProfileFormCardAdd.addEventListener("submit", submitAddCardForm); // обработчик события отправки формы сохранения профиля
+
+btnPopupPhotoClose.addEventListener("click", function() {
+  closePopup(popupPhoto);
 });
